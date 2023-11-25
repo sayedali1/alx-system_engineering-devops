f25c0eb9ba7568412315c6f8ab98b5155ef10df8
saidali1 <said.ali199859@gmail.com>
2023-11-25 12:08:39 +0200

    task100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# config ssh file
+
+file_line { 'trun off pass':
+  ensure => 'present',
+  line   => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+  match  => 'PasswordAuthentication yes',
+}
+
+file_line {'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
